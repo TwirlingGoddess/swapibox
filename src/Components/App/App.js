1ea85fcc43a3137e2cf59e6939eb7be237d61451
Lee Graham <graham.la3@gmail.com>
2018-08-20 18:31:46 -0600

    Create and implement button class components

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,13 +1,17 @@
 import React, { Component } from 'react';
 import './App.css';
 import Landing from '../Landing/Landing';
+import Button from '../Buttons/Button';
 
 class App extends Component {
 	constructor() {
 		super()
 
 		this.state = {
-
+      people: [],
+      planets: [],
+      vehicles: [],
+      favorites: []
 		}
 	}
   render() {
@@ -15,6 +19,10 @@ class App extends Component {
       <div>
         <header className="header">
           <h1 className="title">SWAPIbox</h1>
+          <Button value={this.state.people} />
+          <Button value={this.state.planets} />
+          <Button value={this.state.vehicles} />
+          <Button value={this.state.favorites} />
         </header>
         <Landing />
       </div>
