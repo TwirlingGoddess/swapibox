e9a697e9fad6503dab0c3a451378b1ec1eb5efae
Lee Graham <graham.la3@gmail.com>
2018-08-22 17:35:40 -0600

    Implement conditional rendering for cards

--- src/Components/Card/Card.js
+++ src/Components/Card/Card.js
@@ -2,19 +2,44 @@ import React from 'react';
 import './Card.css';
 import PropTypes from 'prop-types';
 
-export const Card = ({name, species, homeworld, population}) => {
-	
-	return(
-		<div className="Card">
-			<h4>{name}</h4>
-			<hr/>
-			<h5>{species}</h5>
-			<h5>{homeworld}</h5>
-			<h5>{population}</h5>
-			<button>Fav</button>
-		</div>
-	)
-
+export const Card = ({name, species, homeworld, population, terrain, climate, residents, model, carClass, passengers}) => {
+	if(props.state.currentlyDisplayed === 'people'){
+		return(
+			<div className="Card">
+				<h4>{name}</h4>
+				<hr/>
+				<h5>{species}</h5>
+				<h5>{homeworld}</h5>
+				<h5>{population}</h5>
+				<button>Fav</button>
+			</div>
+		)
+	}
+	if(props.state.currentlyDisplayed === 'planets'){
+		return(
+			<div className="Card">
+				<h4>{name}</h4>
+				<hr/>
+				<h5>{terrain}</h5>
+				<h5>{population}</h5>
+				<h5>{climate}</h5>
+				<h5>{residents}</h5>
+				<button>Fav</button>
+			</div>
+		)
+	}
+	if(props.state.currentlyDisplayed === 'vehicles'){
+		return(
+			<div className="Card">
+				<h4>{name}</h4>
+				<hr/>
+				<h5>{model}</h5>
+				<h5>{carClass}</h5>
+				<h5>{passengers}</h5>
+				<button>Fav</button>
+			</div>
+		)
+	}
 }
 
 Card.propTypes = {
