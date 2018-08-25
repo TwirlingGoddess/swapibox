3da6bc67249f1594f6106528a64b98533db26f90
Lee Graham <graham.la3@gmail.com>
2018-08-25 14:32:25 -0600

    Implement tests for the first fetch function

--- src/Components/App/fetchCalls.test.js
+++ src/Components/App/fetchCalls.test.js
@@ -1,28 +1,74 @@
 import React from 'react';
 import Landing from '../Landing/Landing';
-import App from './App'
+import App from './App';
+import { 
+  firstFetch, 
+  fetchData, 
+  fetchHome, 
+  fetchSpecies, 
+  fetchResidents 
+} from './fetchCalls';
 
-beforeEach(() => {
-    mockEvent = jest.fn()
-    mockScroll = 'Turmoil has engulfed the Galactic Republic. The taxation of trade routes to outlying star systems is in dispute.'
+describe ('fetchCalls', () => {
+    let mockEvent;
+    let mockScroll;
+    let mockLandingState;
+    let expected;
+    let url
+    
+  beforeEach(() => {
+    mockEvent = jest.fn();
+    mockScroll = ['Turmoil has engulfed the Galactic Republic. The taxation of trade routes to outlying star systems is in dispute.'];
+    mockLandingState = { text: 'Turmoil has engulfed the Galactic Republic. The taxation of trade routes to outlying star systems is in dispute.' };
     window.fetch = jest.fn().mockImplementation(() => {
       return Promise.resolve({
         ok: true,
         json: () => Promise.resolve(mockScroll)
-      })
-    })
+      });
+    });
   });
 
-  it('should call fetch with the correct params', () => {
-    const expected = [
-    'https://swapi.co/api/films', 
-      {
+  it('should call firstFetch with the correct params', () => {
+    url = 'https://swapi.co/api/films' 
+    expected = [
+      { url,
         method: 'POST' ,
         body: JSON.stringify({
-          scrollText: mockScroll }),
+          scrollText: mockLandingState }),
         headers: {
           'Content-type': 'application/json'
-        }
-      }
-    ]
+        };
+      };
+    ];
+    firstFetch(mockEvent);
+    expect(window.fetch).toHaveBeenCalledWith(...expected);
+  });
+
+  it('should return an object if response is ok', () => {
+    url = 'https://swapi.co/api/films' 
+    expected = mockLandingState
+    const result = await firstFetch(url)
+    expect(result).toEqual(expected)
   })
+
+  it('should throw an error if the fetch fails', async () => {
+    url = 'https://swapi.co/api/films' 
+    expected = new Error(error.message)
+    window.fetch = jest.fn().mockImplementation(() => {
+      return Promise.reject(new Error(error.message))
+    })
+    await expect(firstFetch(url)).rejects.toEqual(expected)
+  })
+
+  it('should throw an error if the status is not ok', async () => {
+    url = 'https://swapi.co/api/films' 
+    expected = new Error(error.message)
+    window.fetch = jest.fn().mockImplementation(() => {
+      return Promise.resolve({
+        ok: false
+      })
+    })
+    await expect(firstFetch(url)).rejects.toEqual(expected)
+  })
+
+})
